bf14dd9ac1f846cb3507e5ace03d7cd5119101e8
Onnichan <dyangeltk17@gmail.com>
2023-07-15 17:43:06 -0500

    initial configuration

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title')</title>
+</head>
+<body>
+
+  @yield('content')
+  @include('layouts.partials.nav')
+  @include('layouts.partials.footer')
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@section('content')
+  Home page section
+  <h1>Salute prros </h1>
+@endsection
